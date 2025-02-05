9d94c25ebedbfecaf4e96e0b0d4def2d2bdeaf02
outofmakarov <6360058@mail.ru>
2025-02-05 20:10:26 +0300

    last commit

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -11,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
 
 
     private Button btnStart, btnStop;
-
+    private MyThread myThread;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +23,55 @@ public class MainActivity extends AppCompatActivity {
 
         btnStart = findViewById(R.id.start_btn);
         btnStop = findViewById(R.id.stop_btn);
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myThread = new MyThread();
+                myThread.isRunning = true;
+                myThread.start();
+            }
+        });
+        btnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myThread.isRunning = false;
+            }
+        });
+    }
+    class MyThread extends Thread{
+        private boolean isRunning = true;
 
+        @Override
+        public void run() {
+            int count = 0;
+            super.run();
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            while (isRunning){
+                if (count % 2 == 0){
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            fragmentManager.beginTransaction().replace(R.id.output_fragment, new ProceedingFragment()).commit();
+
+                        }
+                    });
+                }
+                else{
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            fragmentManager.beginTransaction().replace(R.id.output_fragment, new FirstFragment()).commit();
+
+                        }
+                    });
+                }
+                count++;
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
     }
 }
